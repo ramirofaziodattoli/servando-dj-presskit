0afd097cdc550046dc382942dcfe9bd0a3e92a75
ramirofazio <ramifazio@gmail.com>
2025-06-08 15:53:01 -0300

    fix

--- components/ExternalTags.tsx
+++ components/ExternalTags.tsx
@@ -1,7 +1,35 @@
 "use client";
+import { GA_MEASUREMENT_ID } from "@/lib/gtag";
+import Script from "next/script";
 
 const ExternalTags = () => {
-  return <></>;
+  return (
+    <>
+      {/* Google Analytics Script */}
+      {GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+            }}
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default ExternalTags;
