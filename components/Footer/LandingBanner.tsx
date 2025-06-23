d7353d0979dc6bed4072cb2a04a0696dea8fd061
ramirofazio <ramifazio@gmail.com>
2025-06-23 14:53:53 -0300

    update banner

--- components/Footer/LandingBanner.tsx
+++ components/Footer/LandingBanner.tsx
@@ -5,15 +5,18 @@ export default function LandingBanner() {
   return (
     <div className="relative w-full flex flex-col items-center justify-center h-[100px]">
       <div className="flex items-center justify-center md:justify-start gap-2 w-full section-max-w mx-auto section-px">
-        <Text variant="custom" className="!text-xs whitespace-nowrap">
-          Creado por
+        <Text
+          variant="custom"
+          className="!text-xs whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-tl from-gray-200 to-neutral-600"
+        >
+          POWERED BY
         </Text>
         <Text
           variant="custom"
           className="!text-xs whitespace-nowrap !text-[#59c6ba] font-bold hover:opacity-50 transition duration-500"
         >
           <LandingLink raw href="https://dj-presskit.com/">
-            DJ presskit
+            DJ PRESSKIT
           </LandingLink>
         </Text>
       </div>
